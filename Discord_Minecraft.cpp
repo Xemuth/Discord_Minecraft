b81eb333c6198f9d3181e11026c43c9b1fe934eb
xemuth <hamonclem@hotmail.fr>
2019-07-25 20:25:33 +0200

    Grosse maj sur le format des calls

--- Discord_Minecraft.cpp
+++ Discord_Minecraft.cpp
@@ -3,21 +3,14 @@
 using namespace Upp;
 
 void Discord_Minecraft::launchCommande(ValueMap payload){
-	String channel  = payload["d"]["channel_id"];
-    String content  = payload["d"]["content"];
-    String userName = payload["d"]["author"]["username"];
-    String id = payload["d"]["author"]["id"];
-    String discriminator = payload["d"]["author"]["discriminator"];
-	
-	Cout() << "Event Minecraft" <<"\n";
-	ptrBot->CreateMessage(channel, "Felix est un camé !!");
+	ptrBot->CreateMessage(ChannelLastMessage, "Felix est un camé !!");
 }
 	
 Discord_Minecraft::Discord_Minecraft(Upp::String _name, Upp::String _prefix){
 	name = _name;
 	prefix = _prefix;
 
-	EventsMapMessageCreated.Add([&](ValueMap e){this->launchCommande(e);});
+	EventsMapMessageCreated.Add([&](ValueMap e){if(NameOfFunction.IsEqual("test"))this->launchCommande(e);});
 }
 
 void Discord_Minecraft::EventsMessageCreated(ValueMap payload){
