424b918a891a6b7aaa223cd4fcfcb2f14b84557e
Xemuth <hamon.clement@outlook.fr>
2019-10-17 14:54:13 +0200

    maj RCON

--- Discord_Minecraft.cpp
+++ Discord_Minecraft.cpp
@@ -1,14 +1,24 @@
 #include <Core/Core.h>
 #include "Discord_Minecraft.h"
+
 using namespace Upp;
 
 void Discord_Minecraft::launchCommande(ValueMap payload){
-	ptrBot->CreateMessage(ChannelLastMessage, "Felix est un camé !!");
+	if(	AuthorId.IsEqual("<@131865910121201664>") || AuthorId.IsEqual("<@131915014419382272>")){
+		String command =MessageArgs.ToString();
+		myRcon.SendCommand(command);
+		ptrBot->CreateMessage(ChannelLastMessage, "Commande envoyée");
+	}else{
+		ptrBot->CreateMessage(ChannelLastMessage, "Vous n'avez pas les droits !");
+	}
 }
 	
 Discord_Minecraft::Discord_Minecraft(Upp::String _name, Upp::String _prefix){
 	name = _name;
 	prefix = _prefix;
+	
+	myRcon(String _host, int _port, String _password ,String _serviceName,int _TBR);
+
 
 	EventsMapMessageCreated.Add([&](ValueMap e){if(NameOfFunction.IsEqual("test"))this->launchCommande(e);});
 }
--- Discord_Minecraft.h
+++ Discord_Minecraft.h
@@ -1,13 +1,13 @@
 #ifndef _Discord_Minecraft_Discord_Minecraft_h_
 #define _Discord_Minecraft_Discord_Minecraft_h_
 #include <SmartUppBot/SmartBotUpp.h>
-
+#include <RconManager/RconManager.h>
 using namespace Upp;
 class Discord_Minecraft: public DiscordModule{
 	private:
 	
 		void launchCommande(ValueMap payload);
-		
+		RconManager myRcon;
 	public:
 		
 		Discord_Minecraft(Upp::String _name, Upp::String _prefix);
