fee5d7ae0e923b93c66e7761f38b5fc353795d8a
Xemuth <hamon.clement@outlook.fr>
2019-10-18 12:01:54 +0200

    maj

--- Discord_Minecraft.cpp
+++ Discord_Minecraft.cpp
@@ -4,22 +4,48 @@
 using namespace Upp;
 
 void Discord_Minecraft::launchCommande(ValueMap payload){
-	if(	AuthorId.IsEqual("131865910121201664") || AuthorId.IsEqual("131915014419382272")){
-		String command ="";
-		for(String& e : MessageArgs){
-			command += e  ;
+	if(RconLoaded){
+		if(RconValide){
+			if(RconAuthentified){
+				if(	AuthorId.IsEqual("131865910121201664") || AuthorId.IsEqual("131915014419382272")){
+					String command ="";
+					for(String& e : MessageArgs){
+						command += e  ;
+					}
+					myRcon.SendCommand(command);
+					ptrBot->CreateMessage(ChannelLastMessage, "Commande envoyée");
+				}else{
+					ptrBot->CreateMessage(ChannelLastMessage, "Vous n'avez pas les droits !");
+				}
+			}else{
+				ptrBot->CreateMessage(ChannelLastMessage, "Impossible de s'authentifier sur le serveur. Vérifier le fichier de configuration !");
+			}
+		}else{
+			ptrBot->CreateMessage(ChannelLastMessage, "Impossible d'établir la connexion avec le serveur. Vérifier le fichier de configuration !");
 		}
-		myRcon.SendCommand(command);
-		ptrBot->CreateMessage(ChannelLastMessage, "Commande envoyée");
 	}else{
-		ptrBot->CreateMessage(ChannelLastMessage, "Vous n'avez pas les droits !");
+		ptrBot->CreateMessage(ChannelLastMessage, "Impossible de faire d'envoyer des commandes. Fichier de configuration invalide !");
 	}
 }
 	
-Discord_Minecraft::Discord_Minecraft(Upp::String _name, Upp::String _prefix):myRcon("151.80.10.19",25575, "thunberg","serveur de pierre"){
+Discord_Minecraft::Discord_Minecraft(Upp::String _name, Upp::String _prefix,String RconConfigPath) : rconConfig(RconConfigPath) ,myRcon(rconConfig.GetValue<String>("addr"),rconConfig.GetValue<int>("port"),rconConfig.GetValue<String>("password"),rconConfig.GetValue<String>("serviceName")){
 	name = _name;
 	prefix = _prefix;
-	
+	if(rconConfig.GetValue<String>("addr").GetCount() > 0 && rconConfig.GetValue<int>("port") > 0 && rconConfig.GetValue<String>("password").GetCount() > 0 ){
+		RconLoaded =true;
+		if(myRcon.TestConnexion()){
+			RconValide=true;
+			if(myRcon.TestLoggin()){
+				RconAuthentified=true;
+			}else{
+				LOG("Discord_Minecraft : Rcon authentification failled. All Rcon function will be disable");
+			}
+		}else{
+			LOG("Discord_Minecraft : Rcon connection can't be established. All Rcon function will be disable");
+		}
+	}else{
+		LOG("Discord_Minecraft : Error occured in loading of rconLogs. All Rcon function will be disable");
+	}
 	EventsMapMessageCreated.Add([&](ValueMap e){if(NameOfFunction.IsEqual("command"))this->launchCommande(e);});
 }
 
--- Discord_Minecraft.h
+++ Discord_Minecraft.h
@@ -2,15 +2,20 @@
 #define _Discord_Minecraft_Discord_Minecraft_h_
 #include <SmartUppBot/SmartBotUpp.h>
 #include <RconManager/RconManager.h>
+#include <EasyConfiguration/EasyConfiguration.h>
 using namespace Upp;
 class Discord_Minecraft: public DiscordModule{
 	private:
+		EasyConfiguration rconConfig;
 	
 		void launchCommande(ValueMap payload);
 		RconManager myRcon;
+		bool RconLoaded = false;
+		bool RconValide =false;
+		bool RconAuthentified = false;
 	public:
 		
-		Discord_Minecraft(Upp::String _name, Upp::String _prefix);
+		Discord_Minecraft(Upp::String _name, Upp::String _prefix,String RconConfigPath="");
 		
 		void EventsMessageCreated(ValueMap payload);
 	
